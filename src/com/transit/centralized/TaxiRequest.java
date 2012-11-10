8d41413ba37b164fafff453acf285b2a8bba5de9
Danesh Mondegarian <daneshm90@gmail.com>
2012-11-09 23:41:04 -0500

    Merge socketIO based passenger request code

--- src/com/transit/centralized/TaxiRequest.java
+++ src/com/transit/centralized/TaxiRequest.java
@@ -1,90 +1,177 @@
 package com.transit.centralized;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 import java.util.Locale;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.Toast;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.LinearLayout;
-import android.app.AlertDialog;
-import android.view.MotionEvent;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class TaxiRequest extends Activity {
-	
-	Button requestTaxi;
-	GPSTracker gps;
+import com.codebutler.android_websockets.SocketIOClient;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.taxi);
-		
-		CheckBox onLocation = (CheckBox)findViewById(R.id.checkBox1);
-		final LinearLayout address = (LinearLayout)this.findViewById(R.id.addressLayout);
-		final LinearLayout constantAddress = (LinearLayout)this.findViewById(R.id.constantAddress);
-		onLocation.setOnCheckedChangeListener(new OnCheckedChangeListener()
-		{
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView,boolean isChecked)
-			{
-				if(isChecked)
-				{
-					address.setVisibility(LinearLayout.GONE);
-					constantAddress.setVisibility(LinearLayout.VISIBLE);
-				}
-				else
-				{
-					address.setVisibility(LinearLayout.VISIBLE);
-					constantAddress.setVisibility(LinearLayout.GONE);
-				}
-			}
-			
-		});
-		
-		
-        requestTaxi = (Button) findViewById(R.id.button1);
-        
-        // show location button click event
-        requestTaxi.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {		
-				// create class object
-		        gps = new GPSTracker(TaxiRequest.this);
+public class TaxiRequest extends Activity implements OnClickListener {
 
-				// check if GPS enabled		
-		        if(gps.canGetLocation()){
-		        	Geocoder geocode = new Geocoder(getBaseContext(), Locale.getDefault());
-		        	double latitude = gps.getLatitude();
-		        	double longitude = gps.getLongitude();
-		        	try {
-						List<Address> address = geocode.getFromLocation(latitude,longitude,1);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-		        	
-		        }else{
-		        	// can't get location
-		        	// GPS or Network is not enabled
-		        	// Ask user to enable GPS/network in settings
-		        	gps.showSettingsAlert();
-		        }
-				
-			}
-		});
-	}
+    private final static boolean DEBUG = true;
+    public final static String TAG = "TaxiRequest";
+    SocketIOClient mClient;
+    Button mSendRequest, mResetFields;
+    ImageButton mGeoLocate;
+    EditText mName, mPhoneNumber, mAddress;
+    Context mContext;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.taxi);
+        mSendRequest = (Button) findViewById(R.id.sendrequest);
+        mResetFields = (Button) findViewById(R.id.resetfields);
+        mGeoLocate = (ImageButton) findViewById(R.id.geolocate);
+        mName = (EditText) findViewById(R.id.name);
+        mPhoneNumber = (EditText) findViewById(R.id.phonenumber);
+        mAddress = (EditText) findViewById(R.id.pickupaddress);
+        mContext = this;
+        mSendRequest.setOnClickListener(this);
+        mResetFields.setOnClickListener(this);
+        mGeoLocate.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    String getLocationCoords(String strAddress) {
+        Geocoder coder = new Geocoder(mContext);
+        List<Address> address;
+        try {
+            address = coder.getFromLocationName(strAddress,1);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            return String.valueOf(location.getLatitude() + "|" + location.getLongitude());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void emitMessage(String msg, JSONArray args) {
+        try {
+            mClient.emit(msg, args);
+        } catch (Exception e) {
+            Log.e(TAG, "emitMessage " + msg + " with args " + args.toString(), e);
+        }
+    }
+
+    private void printLog(String msg) {
+        if (DEBUG) {
+            Log.d(TAG, msg);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSendRequest) {
+            mClient = new SocketIOClient(URI.create("http://danesh.it.cx:8080"), new SocketIOClient.Handler() {
+
+                @Override
+                public void onConnect() {
+                    printLog("Connected!");
+                    JSONArray argumentsa = new JSONArray();
+                    argumentsa.put("passengers");
+                    emitMessage("joinroom", argumentsa);
+                    JSONArray jsonArray = new JSONArray();
+                    JSONObject jsonObjects = new JSONObject();
+                    try {
+                        jsonObjects.put("name", mName.getText().toString());
+                        jsonObjects.put("number", mPhoneNumber.getText().toString());
+                        jsonObjects.put("address", getLocationCoords(mAddress.getText().toString()));
+                        jsonArray.put(jsonObjects);
+                        emitMessage("requesttaxi", jsonArray);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void on(String event, final JSONArray arguments) {
+                    printLog(String.format("Got event %s: %s", event, arguments.toString()));
+                    if (event.equals("requestaccepted")) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                                try {
+                                    String driverName = arguments.getJSONObject(0).getString("driver");
+                                    String estimatedTime = arguments.getJSONObject(0).getString("distance");
+                                    builder.setMessage("Taxi driver " + driverName + " will pick you up in " + estimatedTime);
+                                    builder.setPositiveButton("Ok", null);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                builder.show();
+                            }
+                        });
+                    }
+                    try {
+                        mClient.disconnect();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onDisconnect(int code, String reason) {
+                    printLog(String.format("Disconnected! Code: %d Reason: %s", code, reason));
+                }
+
+                @Override
+                public void onError(Exception error) {
+                    Log.e(TAG, "Error!", error);
+                }
+            });
+            mClient.connect();
+        } else if (v == mResetFields) {
+            mName.setText("");
+            mPhoneNumber.setText("");
+            mAddress.setText("");
+        } else if (v == mGeoLocate) {
+            LocationManager locator = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location l = locator.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (l == null) {
+                l = locator.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+            Geocoder geo = new Geocoder(getBaseContext(), Locale.getDefault());
+            try {
+                Address firstAddr = geo.getFromLocation(l.getLatitude(), l.getLongitude(), 1).get(0);
+                mAddress.setText(firstAddr.getAddressLine(0) + " " + firstAddr.getAddressLine(1) + " " + firstAddr.getAddressLine(2));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage("Unable to retrieve location");
+                builder.setPositiveButton("Ok", null);
+                builder.show();
+            }
+        }
+    }
 }
